e1d37f83437094f037f88970501e2914ab88d3d8
olimov Amir <olimov.amir@mail.ru>
2023-05-25 11:58:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingAndUnboxingTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intfive = 5;//Число 5 хранится в stack
+            object objFive = intfive;//Число 5 копируеться 
+
+            int intFive2 = 5;//Число 5 хранится в stack
+            object objFive2 = intFive2;//Число 5 копируеться 
+
+            bool isIntEqual = intfive == intFive2;
+            Console.WriteLine(isIntEqual);//Здесь будет True, потому что 5 ровно 5 
+
+            bool isObjEqual = objFive == objFive2;
+            Console.WriteLine(isObjEqual);//Здесь будет false, потому что пути разные
+           Console.ReadKey();
+        }
+        
+    }
+}
